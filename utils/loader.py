4bee8242c4f08464a85b9b6aaf77f09e255fb203
TobiasWeis <weis.tobi@googlemail.com>
2016-08-21 19:43:16 +0200

    improved plotting

--- utils/loader.py
+++ utils/loader.py
@@ -11,6 +11,9 @@ class Loader():
     def process(self, use_dummies=False, do_filter=False):
         # remove X outliers (everything thats outside 3 standard deviations)
         if do_filter:
+	    print "Before outlier removal: ", len(self.df)
+	    self.df_outliers = self.df[np.abs(self.df.X-self.df.X.mean()) >= (3*self.df.X.std())]
+	    print "Got %d outliers of coordinates, removed." % len(self.df_outliers)
             self.df = self.df[np.abs(self.df.X-self.df.X.mean())<=(3*self.df.X.std())]
 
         self.features = ["X", "Y"]
@@ -39,7 +42,11 @@ class Loader():
 
             print "Done transforming features"
         else:
-            self.df['DayOfWeek_num'] = self.df['DayOfWeek'].astype('category').cat.codes.astype('float')
+	    # define a mapping to get some order in our plots
+	    mapping = {"Monday":1, "Tuesday":2, "Wednesday":3, "Thursday":4, "Friday":5, "Saturday":6, "Sunday":7}
+	    self.df = self.df.replace({'DayOfWeek' : mapping})
+
+            #self.df['DayOfWeek_num'] = self.df['DayOfWeek'].astype('category').cat.codes.astype('float')
             self.features.append("DayOfWeek_num")
 
             self.df['PdDistrict_num'] = self.df['PdDistrict'].astype('category').cat.codes.astype('float')
